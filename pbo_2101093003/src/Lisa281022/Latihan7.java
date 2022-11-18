e9f8baaabdb0ba58fd3d1bcebfa0589286148265
MarlisaGunawan <114122692+MarlisaGunawan@users.noreply.github.com>
2022-11-18 12:13:29 +0700

    Create Latihan7.java

---
+++ pbo_2101093003/src/Lisa281022/Latihan7.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lisa281022;
+
+/**
+ *
+ * @author A416JPO
+ */
+public class Latihan7 {
+    public static void main(String[] args){
+        double a = Double.parseDouble (args[2]);
+        double b = Double.parseDouble (args[3]);
+
+        double sum,difference,product,quotient;
+                
+        sum = a+b;
+        difference = a-b;
+        product = a*b;
+        quotient= a/b;
+        
+        System.out.println("sum = "+ sum );
+        System.out.println("difference = "+ difference );
+        System.out.println("product = "+ product );
+        System.out.println("quotient = "+ quotient );
+    }
+}
