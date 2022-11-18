dbe1f63722b14188c52ff7eecf9ad391203ec91e
MarlisaGunawan <114122692+MarlisaGunawan@users.noreply.github.com>
2022-11-18 12:13:35 +0700

    Create Latihan3.java

---
+++ pbo_2101093003/src/Lisa300922/Latihan3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lisa300922;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author A416JPO
+ */
+public class Latihan3 {
+    public static void main (String[] args){
+        String name = "";
+        name = JOptionPane.showInputDialog("Please enter your name");
+        
+        String msg = "Hello "+ name + "!";
+        
+        JOptionPane.showMessageDialog(null,msg);
+        
+    }
+}
