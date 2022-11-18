fbebbec7017ac1346f5841ef3058c4fbcbe3422d
MarlisaGunawan <114122692+MarlisaGunawan@users.noreply.github.com>
2022-11-18 12:12:03 +0700

    Create HelloWorld.java

---
+++ pbo_2101093003/src/Lisa160922/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lisa160922;
+
+/**
+ *
+ * @author A416JPO
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld\n"+"MI2C\n"+"Pemograman berbasis objek\n");
+        System.out.println("Nama\t:Marlisa Gunawan\n"+"NoBp\t:2101093003\n"+"Prodi\t:Manajemen Informatika\n"+"Jurusan\t:Teknologi Informasi\n"+"Alamat\t:Lubuk Buaya");
+    }
+}
