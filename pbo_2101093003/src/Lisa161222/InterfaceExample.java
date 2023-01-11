c79fde2099ecca4cd462a22e509260154fdb8810
MarlisaGunawan <114122692+MarlisaGunawan@users.noreply.github.com>
2023-01-11 21:54:42 +0700

    Create InterfaceExample.java

---
+++ pbo_2101093003/src/Lisa161222/InterfaceExample.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lisa161222;
+
+/**
+ *
+ * @author A416JPO
+ */
+public class InterfaceExample {
+    public static void main(String[] args) {
+        Relation myInteger = new MyInteger();
+        Relation myDouble =  new MyDouble();
+        
+        boolean hasilInt = myInteger.isGreater(5,6);
+        System.out.println("Hasil MyInteger : "+hasilInt);
+        
+        boolean hasilDbl = myDouble.isGreater(6.4, 2.2);
+        System.out.println("Hasil MyDouble : "+hasilDbl);
+        
+        Relation myline = new Line();
+        Line line1 = new Line(2,5,2,7);
+        Line line2 = new Line(2, 5, 2, 2);
+        
+        boolean lineA_lineB = myline.isGreater(line1,line2);
+        System.out.println("Line A > Line B :"+lineA_lineB);
+        
+    }
+}
