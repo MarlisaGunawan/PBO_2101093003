7895450fff98836732d8b72458e1e7ffd5782c97
MarlisaGunawan <114122692+MarlisaGunawan@users.noreply.github.com>
2023-01-11 21:55:07 +0700

    Create mahasiswaDao.java

---
+++ pbo_2101093003/src/pustaka/model/mahasiswaDao.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pustaka.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author A416JPO
+ */
+public class mahasiswaDao {
+    private List<mahasiswa> data = new ArrayList<mahasiswa>();
+    
+    public mahasiswaDao(){
+        data.add(new mahasiswa("098765","Ali","Padang","L"));
+    }
+    
+    public void insert(mahasiswa m){
+        data.add(m);
+    }
+    
+    public void update(int index, mahasiswa m){
+        data.set(index, m);
+    }
+    
+    public void delete(int index){
+        data.remove(index);
+    }
+    public mahasiswa getMahasiswa(int index){
+        return data.get(index);
+    }
+    
+    public List<mahasiswa> getAllMahasiswa(){
+        return data;
+    }
+}
